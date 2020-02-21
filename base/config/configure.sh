d57c637c19c7299a978523bc8bb5fe7ecc713b6a
Hernán A. Mora <hmora@neosecure.com>
2020-02-20 23:00:07 -0300

    updated configure script

--- base/config/configure.sh
+++ base/config/configure.sh
@@ -3,8 +3,9 @@
 sed -i "s|HADOOP_MASTER|$HADOOP_MASTER|" $HADOOP_HOME/etc/hadoop/core-site.xml
 sed -i "s|HADOOP_MASTER|$HADOOP_MASTER|" $HADOOP_HOME/etc/hadoop/yarn-site.xml
 
-rm $HADOOP_HOME/etc/hadoop/slaves
-IFS=',' read -ra NODES <<< "$SLAVES"
-for NODE in "${NODES[@]}"; do
-    echo "$NODE" >> $HADOOP_HOME/etc/hadoop/slaves
-done
+
+SLAVES_CONFIG="$HADOOP_HOME/etc/hadoop/slaves"
+rm $SLAVES_CONFIG
+if [ ! -z "$SLAVES" ]; then
+    echo $SLAVES | tr , '\n' > $SLAVES_CONFIG
+fi
