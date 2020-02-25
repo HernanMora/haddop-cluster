dc0140e048b64714a99b35aae3e71fdbaceb04a8
Hernán A. Mora <hmora@neosecure.com>
2020-02-25 16:45:54 -0300

    Hadoop v2.9.2

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-docker build --rm -t local/hadoop-base:2.9.1 base
-docker build --rm -t local/hadoop-master:2.9.1 master
+docker build --no-cache --rm -t local/hadoop-base:2.9.2 base
+docker build --no-cache --rm -t local/hadoop-master:2.9.2 master
 
-#docker login registry.devpos.neosecure.com.ar -u admin -p 'Apple2000!'
-
-#docker tag local/hadoop-base:2.9.1 registry.devpos.neosecure.com.ar/local/hadoop-base:2.9.1
-#docker push registry.devpos.neosecure.com.ar/local/hadoop-base:2.9.1
-
-#docker tag local/hadoop-master:2.9.1 registry.devpos.neosecure.com.ar/local/hadoop-master:2.9.1
-#docker push registry.devpos.neosecure.com.ar/local/hadoop-master:2.9.1
